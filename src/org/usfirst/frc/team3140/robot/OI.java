56f818f390f6febf4c0fda7e276fbec64a6b9447
s755234 <s755234@far5wt304-05.knoxschools.ad>
2016-12-01 18:35:47 -0500

    that's a spicy meme

--- src/org/usfirst/frc/team3140/robot/OI.java
+++ src/org/usfirst/frc/team3140/robot/OI.java
@@ -35,6 +35,6 @@ public class OI {
     // until it is finished as determined by it's isFinished method.
     // button.whenReleased(new ExampleCommand());
 	
-	// Hi im sol.
+	// oh yeah Mr. Krabs
 }
 
