c378fb5d2ce6c483b92c60df89ca429acb6b5275
s849443 <s849443@far5wt304-03.knoxschools.ad>
2016-12-01 19:39:37 -0500

    Updated ExampleSubsystem 	- Added rightmotor

--- src/org/usfirst/frc/team3140/robot/subsystems/ExampleSubsystem.java
+++ src/org/usfirst/frc/team3140/robot/subsystems/ExampleSubsystem.java
@@ -10,8 +10,10 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class ExampleSubsystem extends Subsystem {
     
 	TalonSRX leftmotor = new TalonSRX(3);
+	TalonSRX rightmotor = new TalonSRX(1);
 	
     public void initDefaultCommand() {
+    
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
