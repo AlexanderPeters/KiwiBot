68ef747d3970442d88f434b25fb0296d7284daa8
s803182 <s803182@FAR5WT304--02.knoxschools.ad>
2016-12-01 19:36:10 -0500

    Updated Subsystem

--- src/org/usfirst/frc/team3140/robot/subsystems/ExampleSubsystem.java
+++ src/org/usfirst/frc/team3140/robot/subsystems/ExampleSubsystem.java
@@ -1,7 +1,7 @@
 
 package org.usfirst.frc.team3140.robot.subsystems;
 
-import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.TalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -9,12 +9,8 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class ExampleSubsystem extends Subsystem {
     
-	int a = 1;
+	TalonSRX leftmotor = new TalonSRX(3);
 	
-	String mystring=("kys");
-	
-	Spark s;
-
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
