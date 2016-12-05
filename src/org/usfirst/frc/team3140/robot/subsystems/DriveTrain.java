634bf96334cc94d6fb862fae4caabf43fcb8a1bf
s755234 <s755234@far5wt304-05.knoxschools.ad>
2016-12-05 16:41:56 -0500

    added drivetrain

--- src/org/usfirst/frc/team3140/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team3140/robot/subsystems/DriveTrain.java
@@ -1,22 +1,16 @@
-
 package org.usfirst.frc.team3140.robot.subsystems;
 
-import org.usfirst.frc.team3140.robot.RobotMap;
-
-import edu.wpi.first.wpilibj.TalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
- * 
  *
  */
-public class ExampleSubsystem extends Subsystem {
+public class DriveTrain extends Subsystem {
     
-	TalonSRX leftmotor = new TalonSRX(RobotMap.leftMotorA);
-	TalonSRX rightmotor = new TalonSRX(RobotMap.rightMotorA);
-	
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
     public void initDefaultCommand() {
-    
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
